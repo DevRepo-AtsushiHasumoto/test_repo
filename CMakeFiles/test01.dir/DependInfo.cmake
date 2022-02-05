f5a79ab284afb8fa8047398bf3673a7cb1c3bbe9
Atsushi Hasumoto <a.hasumoto@gmail.com>
2022-02-05 23:24:31 +0900

    C++ test code. Hello, test_repo

---
+++ CMakeFiles/test01.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/atsushi/git/test_repo/Sources/main.cpp" "CMakeFiles/test01.dir/Sources/main.cpp.o" "gcc" "CMakeFiles/test01.dir/Sources/main.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Sources/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+  std::cout << "Hello test_repo!!" << std::endl;
+
+
+
+  return 0;
+}
